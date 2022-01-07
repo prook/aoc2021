542acb1d21006162fb6e77051cd67e0cf59364bd
Jan Winkler <winkler@pinerecords.com>
2022-01-07 20:50:01 +0100

    Day 2.

---
+++ src/aoc2021/day2.clj
@@ -0,0 +1,55 @@
+(ns aoc2021.day2
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [aoc2021.util :refer [results]]))
+
+(defn parse-line
+  [s]
+  (let [[m x] (str/split s #" ")]
+    [(keyword m) (parse-long x)]))
+
+(defn part1
+  []
+  (let [movements (with-open [r (io/reader "input/day2")]
+                    (->> r
+                         (line-seq)
+                         (mapv parse-line)))
+        {:keys [position depth]} (reduce (fn [acc [m x]]
+                                           (cond-> acc
+                                                   (= m :forward) (update :position + x)
+                                                   (= m :up) (update :depth - x)
+                                                   (= m :down) (update :depth + x)))
+                                         {:position 0
+                                          :depth    0}
+                                         movements)]
+    (* position depth)))
+
+(defn forward
+  [{:keys [aim position depth]} x]
+  {:aim      aim
+   :position (+ position x)
+   :depth    (+ depth (* x aim))})
+
+(defn part2
+  []
+  (let [movements (with-open [r (io/reader "input/day2")]
+                    (->> r
+                         (line-seq)
+                         (mapv parse-line)))
+        {:keys [position depth]} (reduce (fn [acc [m x]]
+                                           (cond-> acc
+                                                   (= m :up) (update :aim - x)
+                                                   (= m :down) (update :aim + x)
+
+                                                   (= m :forward) (forward x)))
+
+                                         {:aim      0
+                                          :position 0
+                                          :depth    0}
+                                         movements)]
+    (* position depth)))
+
+
+(results "Day 1"
+         (part1)
+         (part2))
